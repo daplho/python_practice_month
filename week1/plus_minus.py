00b37f0c1f63af9d4767cc0ba616db924ea24864
daplho <nskirov@gmail.com>
2022-02-23 11:44:25 -0800

    first commit

---
+++ week1/plus_minus.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    positive = 0
+    negative = 0
+    zero = 0
+
+    for num in arr:
+        if num > 0:
+            positive += 1
+        elif num < 0:
+            negative += 1
+        else:
+            zero += 1
+
+    print("{0:.6f}".format(positive/len(arr)))
+    print("{0:.6f}".format(negative/len(arr)))
+    print("{0:.6f}".format(zero/len(arr)))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
