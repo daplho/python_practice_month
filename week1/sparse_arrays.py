ee87f5ac29579988cad8814681a8674cc9ebd4b9
daplho <nskirov@gmail.com>
2022-03-07 14:22:07 -0800

    * implemented sparse_arrays in python

---
+++ week1/sparse_arrays.py
@@ -0,0 +1,44 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    results = []
+
+    for q in queries:
+        results.append(strings.count(q))
+
+    return results
+                
+
+if __name__ == '__main__':
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    print('\n'.join(map(str, res)))
