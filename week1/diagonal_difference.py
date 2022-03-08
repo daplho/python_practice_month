8c823bf27f4131d76588ca06ea52550278c630c6
daplho <nskirov@gmail.com>
2022-03-08 12:27:34 -0800

    * implemented diagonal_difference

---
+++ week1/diagonal_difference.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    d1 = 0
+    d2 = 0
+
+    n = len(arr[0])
+    for i in range(n):
+        d1 += arr[i][i]
+        d2 += arr[n - i - 1][i]
+
+    return abs(d1 - d2)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    print(str(result) + '\n')
