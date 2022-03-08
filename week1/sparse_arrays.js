40844f342beed698c922da33b37b9f777fbbc585
daplho <nskirov@gmail.com>
2022-03-08 10:47:24 -0800

    * implemented sparse arrays in javascript

---
+++ week1/sparse_arrays.js
@@ -0,0 +1,71 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    let counts = []
+    for (let i = 0; i < queries.length; i++) {
+        counts[i] = 0;
+    }
+
+    for (let i = 0; i < queries.length; i++) {
+        for (let s of strings) {
+            if (queries[i] === s) {
+                counts[i]++
+            }
+        }
+    }
+
+    return counts
+}
+
+function main() {
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    console.log(res.join('\n') + '\n');
+}
