76dbe9cfda5457f89fc30ada19cde668c8966489
daplho <nskirov@gmail.com>
2022-03-15 10:06:30 -0700

    * implemented an efficient balanced sums

---
+++ week2/balanced_sums.py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    sumLeft = 0
+    sumRight = sum(arr)
+    for i in range(len(arr)):
+        if i > 0:
+            sumLeft += arr[i-1]
+        if i < len(arr):
+            sumRight -= arr[i]
+
+        if sumLeft == sumRight:
+            return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        print(result + '\n')
