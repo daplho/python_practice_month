d2537d2a18325b9f1cad2aa001ea179a2b38f1a5
daplho <nskirov@gmail.com>
2022-04-08 16:01:14 -0700

    * implemented a min heap with insert

---
+++ week4/heap.py
@@ -0,0 +1,42 @@
+import math
+import sys
+
+class Heap():
+    def __init__(self, fptr):
+        self.fptr = fptr
+        self.h = []
+
+    def insert(self, x):
+        self.h.append(x)
+        self.sift_up(len(self.h) - 1)
+    
+    def delete(self, x):
+        return
+
+    def print_min(self):
+        self.fptr.write(str(self.h[0]) + '\n')
+
+    def sift_up(self, i):
+        while i > 0:
+            parent_i = math.floor((i-1)/2)
+            if self.h[i] < self.h[parent_i]:
+                self.h[i], self.h[parent_i] = self.h[parent_i], self.h[i]
+                i = parent_i
+            else:
+                return
+
+if __name__ == "__main__":
+    fptr = open(sys.stdout.fileno(), 'w')
+
+    q = int(input())
+
+    heap = Heap(fptr)
+    for q_itr in range(q):
+        query = list(map(int, input().split(' ')))
+
+        if query[0] == 1:
+            heap.insert(query[1])
+        if query[0] == 2:
+            heap.delete(query[1])
+        if query[0] == 3:
+            heap.print_min()
